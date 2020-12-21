b0452d61e8369592c69b9342748e3b1c33d1a153
single-byte <70197335+single-byte@users.noreply.github.com>
2020-12-21 21:47:39 +0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -45,3 +45,35 @@ function showAlert(type, msg, time = 2) {
   window.setTimeout(hideAlert, time * 1000);  //multiply user sec with 1000 to conv to millisecs
 };
 
+// to lead voices array
+document.addEventListener("DOMContentLoaded", function (event) {
+  var voices = window.speechSynthesis.getVoices();
+});
+// as gogle cant speak lon texts so timeout for that
+var myTimeout;
+function myTimer() {
+  window.speechSynthesis.pause();
+  window.speechSynthesis.resume();
+  myTimeout = setTimeout(myTimer, 10000);
+}
+// reading email func
+function readEmail() {
+  window.speechSynthesis.cancel();
+  myTimeout = setTimeout(myTimer, 10000);
+  var msg = new SpeechSynthesisUtterance();
+  var voices = window.speechSynthesis.getVoices();
+  msg.rate = 1.1;
+  msg.volume = 0.5;
+  msg.pitch = 0.9;
+  msg.voice = voices[10];
+  var body = document.querySelector('.sender-body').textContent;
+  var subject = document.querySelector('.inbox-subject').textContent;
+  var email = document.querySelector('.sender-name').textContent;
+  msg.text = `this email is from: ${email}. Subject line is:  ${subject}. Message is: ${body}`;
+  msg.onend = function () { clearTimeout(myTimeout); }
+  window.speechSynthesis.speak(msg);
+}
+
+function stopread() {
+  window.speechSynthesis.cancel();
+}
