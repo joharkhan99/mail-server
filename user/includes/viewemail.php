7705ddbe0ecb631b0ef34df0af814dc525933b60
single-byte <70197335+single-byte@users.noreply.github.com>
2020-12-21 21:48:45 +0500

    Update viewemail.php

--- user/includes/viewemail.php
+++ user/includes/viewemail.php
@@ -23,17 +23,21 @@ if (isset($_GET['source'])) {
 ?>
 <div class="container viewEmail">
   <div class="inbox-table">
-    <h1><?php echo !empty($row['subject']) ? $row['subject'] : 'No Subject Line'; ?></h1>
+    <h1 class="inbox-subject"><?php echo !empty($row['subject']) ? $row['subject'] : 'No Subject Line'; ?></h1>
     <div class="sender-info">
       <div class="sender-img">
         <img src="../img/<?php echo Defaultimage(getUserImage($row['fromEmail'])); ?>" alt="">
       </div>
-      <span class="sender-name"><?php echo getUserName($row['fromEmail']); ?> <small><?php echo "&lt;" . $row['fromEmail'] . "&gt;"; ?></small>
-        <span class="sender-date"><?php echo $row['date']; ?></span>
-      </span>
+      <span class="sender-name"><?php echo getUserName($row['fromEmail']); ?></span>
+      <small><?php echo "&lt;" . $row['fromEmail'] . "&gt;"; ?></small>
+      <span class="sender-date"><?php echo $row['date']; ?></span>
       <p class="sender-body">
         <?php echo $row['message']; ?>
       </p>
+      <div class="speaker">
+        <i class="fas fa-volume-up" title="read email" onclick="readEmail()"></i>
+        <i class="fas fa-volume-mute" title="stop reading" onclick="stopread()"></i>
+      </div>
     </div>
   </div>
 </div>
